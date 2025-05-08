f23adec85f17c34f9fb75b74431ad5d7cdc46611
Niyaah31 <saranithavam@gmail.com>
2025-05-08 20:25:26 +0530

    completed all

--- src/core concept/Taskmanager/Taskform.jsx
+++ src/core concept/Taskmanager/Taskform.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
 
 
 
-export default function TaskForm (){
+export default function TaskForm ({addTask}){
+    const [value,setvalue] = useState("");
+        const handle = (event)=>{
+        event.preventDefault();
+        // prevent the input as a spaces
+        if (value.trim()) {
+           // console.log('Submitted',value)
+            //add task
+            addTask(value);
+            // refresh the txt box
+            setvalue("")
+        }
+       
+    }
     return <>
-    <form action="">
+    <form action="" onSubmit={handle}>
         <div className="task-action">
         <input type="text" 
+        onChange={(event)=> setvalue(event.target.value)}
+        // refresh the tctbx
+        value={value}
          placeholder="Add task" className="form-txt"/> 
         <input type="submit" value="Add"  className="form-add-btn"/>
         </div>
--- src/core concept/Taskmanager/Taskmanager.jsx
+++ src/core concept/Taskmanager/Taskmanager.jsx
@@ -1,13 +1,32 @@
 import { useState } from "react"
 import TaskForm from "./Taskform"
 import './task.css'
+import TaskList from "./Tasklist";
 
 export default function Taskmanager(){
-    const [task,settask] = useState([]);
+  const [task,settask] = useState([]);
 
+ const addTask = (text) => {
+const newTask = {id:Date.now(),text,completed:false};
+settask([...task,newTask])
+}
+// for added task completed function
+const toggleTask = (id)=> {
+    settask(task.map((t)=>{
+        // condition{true}:false
+       return t.id == id ? {...t,completed:!t.completed}: t
+    }))
+}
+// delete function
+const deleteTask = (id) =>{
+const filteredTask = task.filter((t) =>{
+     return t ['id'] !== id });
+     settask(filteredTask);
+}
     return <div>
         <h1>Task Manager</h1>
-        <TaskForm/>
+        <TaskForm  addTask={addTask}/>
+       <TaskList task={task} toggleTask ={toggleTask} deleteTask={deleteTask} />
          Task: {task.length} 
     </div>
 }
